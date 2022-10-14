1a731ccce2821705535f78d6334b81a9d6373074
Matthew Duffy <mattduffy@gmail.com>
2022-10-13 20:31:31 -0700

    Add getMetadata method to extract exif data from images.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
  * @module @mattduffy/exiftool
+ * @author Matthew Duffy <mattduffy@gmail.com>
  */
 
 import { stat } from 'node:fs/promises'
@@ -9,8 +10,8 @@ const util = require('node:util')
 const cmd = util.promisify(require('node:child_process').exec)
 
 /**
- * @todo [ ] create a class constructor method to initialize exiftool
- * @todo [ ] create an options object to set exiftool output behavior
+ * @todo [x] create a class constructor method to initialize exiftool
+ * @todo [x] create an options object to set exiftool output behavior
  * @todo [ ] create class method to extract metadata using custom shortcut
  * @todo [ ] create class method to extract all metadata
  * @todo [ ] create a class method to extract arbitrary metadata
@@ -20,7 +21,8 @@ const cmd = util.promisify(require('node:child_process').exec)
 /**
  * A class wrapping the exiftool metadata tool.
  * @summary A class wrapping the exiftool image metadata extraction tool.
- * @author mattduffy@gmail.com
+ * @class Exiftool
+ * @author Matthew Duffy <mattduffy@gmail.com>
  * @module @mattduffy/exiftool
  */
 export default class Exiftool {
@@ -36,17 +38,19 @@ export default class Exiftool {
     this._opts.exif_config = `-config ${this._cwd}/exiftool.config`
     this._opts.outputFormat = `-json`
     this._opts.gpsFormat = `-c "%.6f"`
+    this._opts.shortcut = `-MattsShortcut`
     this._opts.includeTagFamily = `-G`
     this._opts.compactFormat = `-s3`
     this._opts.quite = `-q`
     this._opts.excludeTypes = `--ext TXT --ext JS  --ext JSON  --ext MJS --ext CJS --ext MD --ext HTML`
     this._command = `${this._executable} ${this.getOptions()} ${this._path}`
-    
   }
 
   /**
    * Initializes some asynchronus properties.
    * @summary Initializes some asynchronus class properties not done in the constructor.
+   * @author mattduffy@gmail.com
+   * @async
    * @param { string } - A file system path to set for exiftool to process.
    * @return { Exiftool|boolean } - Returns fully initialized instance or false.
    */
@@ -75,32 +79,43 @@ export default class Exiftool {
   /**
    * Set the path for image file or directory of images to process with exiftool.
    * @summary Set the path of image file or directory of images to process with exiftool.
+   * @author mattduffy@gmail.com
+   * @async
    * @param { string } - A file system path to set for exiftool to process.
-   * @return { Object} - Returns an object literal with success or error messages.
+   * @return { Object } - Returns an object literal with success or error messages.
    */
   async setPath( path ) {
-    let o = {val: null, err: null}
+    let o = {value: null, error: null}
     try {
       this._path = path
       this._fileStats = await stat(path)
       this._isDirectory = this._fileStats.isDirectory()
-      o.val = true
+      this._command = `${this._executable} ${this.getOptions()} ${this._path}`
+      o.value = true
     } catch (e) {
       debug(e)
-      o.err = e
+      o.error = e.message
+      o.errorCode = e.code
+      o.errorStack = e.stack
     }
     return o
   }
 
   /**
-   * Concatenate all the exiftool options together into a single string
+   * Concatenate all the exiftool options together into a single string.
+   * @summary Concatenate all the exiftool options together into a single string.
+   * @author mattduffy@gmail.com
    * @return { string } - Commandline options to exiftool.
    */
   getOptions() {
     return Object.values(this._opts).join(' ')
   }
+
   /**
    * Find the path to the executable exiftool binary.
+   * @summary Find the path to the executable exiftool binary.
+   * @author mattduffy@gmail.com
+   * @async
    * @return { string|boolean } - File system path to exiftool binary or FALSE if not found.
    */
   async which() {
@@ -111,6 +126,28 @@ export default class Exiftool {
       }
     } catch (e) {
       return false
+    }
+  }
+
+  /**
+   * Run the composed exiftool commend to get the requested exif metadata.
+   * @summary Get the exif metadata for one or more image files.
+   * @author Matthew Duffy <mattduffy@gmail.com>
+   * @async
+   * @return { Object|Error } - JSON object literal of metadata or Error if failed.
+   */
+  async getMetadata() {
+    try {
+      let metadata = await cmd(this._command)
+      if ('' != metadata.stderr) {
+        throw new Exeception(metadata.stderr)
+      }
+      metadata = JSON.parse(metadata.stdout)
+      debug(metadata)
+      return metadata
+    } catch (e) {
+      debug(e)
+      return e
     }
   }
 }
