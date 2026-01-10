ee3f2a535f5c5b617106b2258d3616d6bda294d9
Matthew Duffy <mattduffy@gmail.com>
2026-01-09 19:30:15 -0800

    Add eslint-import-resolver-exports package.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,53 @@
+const restrictedGlobals = require('eslint-restricted-globals')
+
 module.exports = {
+  settings: {
+    'import/resolver': {
+      exports: {},
+      node: {
+        extensions: ['.js', '.mjs'],
+      },
+    },
+  },
+  globals: {
+    window: true,
+    document: true,
+    origin: true,
+    worker: true,
+  },
   env: {
     es2021: true,
     node: true,
+    browser: true,
   },
-  extends: [
-    'airbnb-base',
+  plugins: [
+  ],
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      files: ['public/j/worker.js'],
+      rules: {
+        'no-restricted-globals': ['error', 'isFinite', 'isNaN'].concat(restrictedGlobals),
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+    'no-use-before-define': ['error', {
+      functions: false,
+      classes: false,
+      variables: true,
+      allowNamedExports: true,
+    }],
     semi: ['error', 'never'],
     'no-console': 'off',
     'no-underscore-dangle': 'off',
+    'import/extensions': 'off',
     'import/prefer-default-export': 'off',
-    'max-len': 'off',
+    'max-len': ['error', { code: 100 }],
+    'new-cap': 'off',
   },
 }
